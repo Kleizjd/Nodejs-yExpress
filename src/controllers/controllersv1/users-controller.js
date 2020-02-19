f9c18553cc340e0b29b288b67c3d656b740a96d0
Jose Daniel Grijalba Osorio <jjdd1997@hotmail.com>
2020-02-19 10:31:06 -0500

    tercer commit

--- src/controllers/controllersv1/users-controller.js
+++ src/controllers/controllersv1/users-controller.js
@@ -1,18 +1,27 @@
-const createUser = (request, response) => {
-  console.log("request.body", request.body);
-  response.send({ status: "OK", message: "user created" });
+const bcrypt = require('bcrypt');
+
+const createUser = async (request, response) => {
+
+  try {
+    console.log('request.body', request.body);
+    const hash = await bcrypt.hash(request.body.password, 15);
+    console.log('fin', hash);
+
+    response.send({ status: 'OK', message: 'user created' });
+  } catch (error) {
+    response.status(500).send({ status: 'Error', message: error.message });
+  }
 };
 const updateUser = (request, response) => {
-  response.send({ status: "OK", message: "user update" });
+  response.send({ status: 'OK', message: 'user update' });
 };
 const deleteUser = (request, response) => {
-  console.log("req-body", request.body);
-  response.send({ status: "OK", message: "user deleted" });
+  console.log('req-body', request.body);
+  response.send({ status: 'OK', message: 'user deleted' });
 };
 
 const getUsers = (request, response) => {
-  response.send({ status: "OK", data: [] });
+  response.send({ status: 'OK', data: [] });
 };
 
-
-module.exports = {createUser, deleteUser, getUsers, updateUser};
+module.exports = { createUser, deleteUser, getUsers, updateUser };
--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const routesV1 = require('./routes/v1');
 
 const app = express();
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
- 
+
 // parse application/json
 app.use(bodyParser.json());
 
-
 routesV1(app);
- 
+
 app.listen(4000, () => {
- console.log("running on 4000");
+  console.log('running on 4000');
 });
